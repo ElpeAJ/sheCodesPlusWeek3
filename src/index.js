070945b0b10b65a9b011a6ef8704bc9bc590c71b
Elpedia Arthur <elpediaarthurjunior@gmail.com>
2023-12-31 07:19:27 -0800

    Changes to JS file

--- src/index.js
+++ src/index.js
@@ -19,50 +19,64 @@ let weather = {
     temp: -5,
     humidity: 20,
   },
+  accra: {
+    temp: 29,
+    humidity: 5,
+  },
 };
 
 // write your code here
 
-let enterCity = prompt("Please enter City ");
-enterCity = enterCity.toLowerCase();
+function checkCity() {
+  let enterCity = prompt("Please enter City ");
+  enterCity = enterCity.toLowerCase();
 
-if (enterCity === "paris") {
-  alert(
-    `It is currently ${Math.round(
-      weather.paris.temp
-    )}°C in ${enterCity} with a humidity of ${weather.paris.humidity}%`
-  );
-} else if (enterCity === "tokyo") {
-  alert(
-    `It is currently ${Math.round(
-      weather.tokyo.temp
-    )}°C in ${enterCity} with a humidity of ${weather.tokyo.humidity}%`
-  );
-} else if (enterCity === "lisbon") {
-  alert(
-    `It is currently ${Math.round(
-      weather.lisbon.temp
-    )}°C in ${enterCity} with a humidity of ${weather.lisbon.humidity}%`
-  );
-} else if (enterCity === "san francisco") {
-  alert(
-    `It is currently ${Math.round(
-      weather["san francisco"].temp
-    )}°C in ${enterCity} with a humidity of ${
-      weather["san francisco"].humidity
-    }%`
-  );
-} else if (enterCity === "oslo") {
-  alert(
-    `It is currently ${Math.round(
-      weather.oslo.temp
-    )}°C in ${enterCity} with a humidity of ${weather.oslo.humidity}%`
-  );
-} else {
-  alert(
-    "We do not have information for this citySorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
-  );
+  if (enterCity === "paris") {
+    alert(
+      `It is currently ${Math.round(
+        weather.paris.temp
+      )}°C in ${enterCity} with a humidity of ${weather.paris.humidity}%`
+    );
+  } else if (enterCity === "tokyo") {
+    alert(
+      `It is currently ${Math.round(
+        weather.tokyo.temp
+      )}°C in ${enterCity} with a humidity of ${weather.tokyo.humidity}%`
+    );
+  } else if (enterCity === "lisbon") {
+    alert(
+      `It is currently ${Math.round(
+        weather.lisbon.temp
+      )}°C in ${enterCity} with a humidity of ${weather.lisbon.humidity}%`
+    );
+  } else if (enterCity === "san francisco") {
+    alert(
+      `It is currently ${Math.round(
+        weather["san francisco"].temp
+      )}°C in ${enterCity} with a humidity of ${
+        weather["san francisco"].humidity
+      }%`
+    );
+  } else if (enterCity === "oslo") {
+    alert(
+      `It is currently ${Math.round(
+        weather.oslo.temp
+      )}°C in ${enterCity} with a humidity of ${weather.oslo.humidity}%`
+    );
+  } else if (enterCity === "accra") {
+    alert(
+      `It is currently ${Math.round(
+        weather.accra.temp
+      )}°C in ${enterCity} with a humidity of ${weather.accra.humidity}%`
+    );
+  } else {
+    alert(
+      "We do not have information for this city. Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
+    );
+  }
 }
 
+checkCity();
+// let footer = document.querySelector("footer");
+// footer.insertAdjacentHTML("afterend", "Hi there");
 // enterCities();
-// checkCity();
